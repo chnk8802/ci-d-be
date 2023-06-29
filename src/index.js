d01bc9205632074c900386158b8bdc8c25dfdaa8
chnk8802 <chnk8802@gmail.com>
2023-06-29 12:24:43 +0530

    ftch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fetch from 'node-fetch';
 import 'dotenv/config';
 import { connectDB } from './db/db';
 import { Crypto } from './model/crypto';
