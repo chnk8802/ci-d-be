cf953593c5d8cfd5d0c649573dc1283ca673a04f
chnk8802 <chnk8802@gmail.com>
2023-06-29 12:35:31 +0530

    ax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import fetch from 'node-fetch';
+import axios from 'axios';
 import 'dotenv/config';
 import { connectDB } from './db/db';
 import { Crypto } from './model/crypto';
@@ -11,36 +11,35 @@ const app = express();
 app.use(cors());
 
 const getData = () => {
-    try {
-    fetch('https://api.wazirx.com/api/v2/tickers')
-    .then(response => response.json())
-    .then(async result => {
+  try {
+    axios.get('https://api.wazirx.com/api/v2/tickers')
+      .then(async (response) => {
+        const result = response.data;
         var i = 0;
         for (const key in result) {
-            i++;
-            if (Object.hasOwnProperty.call(result, key)) {
-                const element = result[key];
-                const crypto = new Crypto(element);
-                await crypto.save()
-                if (i==10)
-                {
-                    break;
-                }
+          i++;
+          if (Object.hasOwnProperty.call(result, key)) {
+            const element = result[key];
+            const crypto = new Crypto(element);
+            await crypto.save();
+            if (i == 10) {
+              break;
             }
+          }
         }
-    })
-    } catch (err) {
-        console.log("ERROR:( " + err + " )");
-    }
-    
+      });
+  } catch (err) {
+    console.log("ERROR:(" + err + ")");
+  }
 }
+
 const emptyDB = async (callback) => {
-    if (await Crypto.find({})) {
-        Crypto.collection.drop();
-        callback();
-    } else {
-        callback();
-    }
+  if (await Crypto.find({})) {
+    Crypto.collection.drop();
+    callback();
+  } else {
+    callback();
+  }
 }
 
 emptyDB(getData);
@@ -48,12 +47,81 @@ emptyDB(getData);
 setInterval(() => emptyDB(getData), 30000);
 
 // Define a simple route
-app.get('/cryptoinfo/api/v1/tickers', async (req, res) => {
+app.get('/', async (req, res) => {
     res.json(await Crypto.find({}));
+  });
+
+// Define a simple route
+app.get('/cryptoinfo/api/v1/tickers', async (req, res) => {
+  res.json(await Crypto.find({}));
 });
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server-Connected@:${PORT}`);
 });
+
+
+
+// ----------------------------------------------------------
+
+
+// import express from 'express';
+// import cors from 'cors';
+// import fetch from 'node-fetch';
+// import 'dotenv/config';
+// import { connectDB } from './db/db';
+// import { Crypto } from './model/crypto';
+
+// connectDB();
+// const PORT = process.env.PORT;
+// const app = express();
+// app.use(cors());
+
+// const getData = () => {
+//     try {
+//     fetch('https://api.wazirx.com/api/v2/tickers')
+//     .then(response => response.json())
+//     .then(async result => {
+//         var i = 0;
+//         for (const key in result) {
+//             i++;
+//             if (Object.hasOwnProperty.call(result, key)) {
+//                 const element = result[key];
+//                 const crypto = new Crypto(element);
+//                 await crypto.save()
+//                 if (i==10)
+//                 {
+//                     break;
+//                 }
+//             }
+//         }
+//     })
+//     } catch (err) {
+//         console.log("ERROR:( " + err + " )");
+//     }
+    
+// }
+// const emptyDB = async (callback) => {
+//     if (await Crypto.find({})) {
+//         Crypto.collection.drop();
+//         callback();
+//     } else {
+//         callback();
+//     }
+// }
+
+// emptyDB(getData);
+// // automatically updates data every 30sec
+// setInterval(() => emptyDB(getData), 30000);
+
+// // Define a simple route
+// app.get('/cryptoinfo/api/v1/tickers', async (req, res) => {
+//     res.json(await Crypto.find({}));
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server-Connected@:${PORT}`);
+// });
 
