83f3958618bf6d1952c490d2514c4661de252da6
chnk8802 <chnk8802@gmail.com>
2023-06-29 12:14:41 +0530

    some

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = () => {
     // Connect to MongoDB using Mongoose
-    const urlProd = `${process.env.DB_URL}`
+    // const urlProd = `${process.env.DB_URL}`
+    const urlProd = "mongodb+srv://chnk:XEqM0HCwkQHP8XmP@hod-db.awb7lwy.mongodb.net/"
     mongoose.connect(urlProd, {
       useNewUrlParser: true,
     })
